dfba7ad48c30cd846b65c250591d2d8d887e1095
Jean Boussier <jean.boussier@gmail.com>
2022-04-13 11:33:42 +0200

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for file in `ls $DIR | grep -v setup.sh`; do
+for file in `ls files/ | grep -v setup.sh`; do
   rm -rf "$HOME/.$file"
-  cp -vR "$DIR/$file" "$HOME/.$file"
+  cp -vR "files/$file" "$HOME/.$file"
 done
